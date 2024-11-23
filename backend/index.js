2e1acff83e0ed609c29bba5eb8efd100e4ba7902
Dharmik Patel <pateldharmik4701@gmail.com>
2024-11-23 10:51:27 +0530

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(() => { console.log("Connected to DB") })
+    .catch((err) => { console.log("DB Error", err) })
 
 const PORT = process.env.PORT || 3000
 const app = express();
