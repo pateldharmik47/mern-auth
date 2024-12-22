b0221096776860426a668fe0802e7c60482e214e
Dharmik Patel <pateldharmik4701@gmail.com>
2024-12-22 20:44:20 +0530

    remove log from file

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -4,7 +4,6 @@ import { errorHandler } from "../utils/error.js";
 
 export const verfiyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-    console.log(token);
 
     if (!token) {
         return next(errorHandler(401, "You are not authorized to access this route"));
