e6dbae2347a8f520afc8f8d704e509ccdd1d5357
Dharmik Patel <pateldharmik4701@gmail.com>
2024-12-31 07:05:36 +0530

    add pre production changes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +13,12 @@ mongoose.connect(process.env.MONGO)
     .then(() => { console.log("Connected to DB") })
     .catch((err) => { console.log("DB Error", err) })
 
+const __dirname = path.resolve();
 const app = express();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use(express.json())
 app.use(cookieParser());
 
